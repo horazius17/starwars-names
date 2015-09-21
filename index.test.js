c08248b3d1465f29dba4acf7ec664c7c7441be4e
Daniel Schmidt <horazius@outlook.com>
2015-09-22 01:44:14 +0200

    Improved Test

--- index.test.js
+++ index.test.js
@@ -2,7 +2,16 @@
 var starWars = require("./index");
 
 describe("horazius-starwars-names", function () {
-    it ("should work", function () {
-        expect(true).to.be.true;
+    describe("all", function () {
+        
+        it("should be an array of strings", function () {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function (item) {
+                    return typeof item === "string";
+                });
+            }
+        });
     });
 });
