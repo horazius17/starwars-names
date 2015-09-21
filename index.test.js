552c628353ce7c704f566b029f0bf3435bd09e21
Daniel Schmidt <horazius@outlook.com>
2015-09-22 01:34:28 +0200

    Test hinzugefügt

---
+++ index.test.js
@@ -0,0 +1,8 @@
+﻿var expect = require("chai").expect;
+var starWars = require("./index");
+
+describe("horazius-starwars-names", function () {
+    it ("should work", function () {
+        expect(true).to.be.true;
+    });
+});
